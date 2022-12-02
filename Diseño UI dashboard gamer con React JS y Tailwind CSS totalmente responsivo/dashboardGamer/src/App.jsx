e98b0aaa74140338745aaea6d54a563f27fe3429
DvidGs <dgallegos9207@gmail.com>
2022-12-02 16:20:54 -0500

    DashboardGamer-2

--- Diseño UI dashboard gamer con React JS y Tailwind CSS totalmente responsivo/dashboardGamer/src/App.jsx
+++ Diseño UI dashboard gamer con React JS y Tailwind CSS totalmente responsivo/dashboardGamer/src/App.jsx
@@ -12,6 +12,9 @@ import {
   RiCloseLine,
   RiNotification3Line,
   RiSearchLine,
+  RiArrowRightSLine,
+  RiArrowLeftCircleLine,
+  RiArrowRightCircleLine,
 } from "react-icons/ri"
 
 function App() {
@@ -24,7 +27,7 @@ function App() {
   return (
     <div className="bg-[#141414] min-h-screen">
       <sidebar 
-        className={`fixed top-0 w-80 h-full overflow-y-scroll border-r border-gray-800 p-8 flex flex-col justify-between bg-[#141414] transition-all lg:left-0
+        className={`fixed top-0 w-80 h-full overflow-y-scroll border-r border-gray-800 p-8 flex flex-col justify-between bg-[#141414] transition-all lg:left-0 z-50
           ${showMenu ? "left-0" : "-left-full"
         }`}
       >
@@ -93,13 +96,13 @@ function App() {
           </ul>
         </div>
         {/* Btn menu movil */}
-        <button onClick={toggleMenu} className="lg:hidden bg-blue-600 text-white fixed bottom-8 right-6 p-2 text-lg rounded-full">
+        <button onClick={toggleMenu} className="lg:hidden bg-blue-600 text-white fixed bottom-8 right-6 p-2 text-lg rounded-full z-50">
           {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
         </button>
       </sidebar>
       {/* Header */}
-      <header className="fixed pl-[340px] w-full flex items-start justify-between p-8">
-        <nav className="flex items-center gap-4">
+      <header className="fixed lg:pl-[340px] w-full flex flex-col md:flex-row items-center justify-between gap-4 p-8 bg-[#141414] z-40">
+        <nav className="flex items-center gap-4 order-1 lg:order-none">
           <a href="#" className="bg-blue-600 text-white py-2 px-4 rounded-full">
             Discover
           </a>
@@ -119,12 +122,89 @@ function App() {
           </li>
           <li>
             <form className="relative">
-              <RiSearchLine className="text-gray-500 absolute" />
-              <input type="text" className="bg-[#232323] outline-none text-gray-300" />
+              <RiSearchLine className="text-gray-500 absolute top-3 left-2" />
+              <input type="text" className="bg-[#232323] outline-none text-gray-300 py-2 pl-8 pr-4 rounded-full" placeholder="Search" w-full />
             </form>
           </li>
         </ul>
       </header>
+      {/* Content */}
+      <main className="lg:pl-[340px] p-8 pt-36">
+        {/* Hero */}
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-2">
+          <div className="md:col-span-5 relative">
+            <img 
+              src="https://cdn2.unrealengine.com/Fortnite%2Fblog%2Fsolo-showdown-limited-time-mode%2FFN_News_Large_EPIC-GAMES-WILL-PROVIDE-%24100-1920x1080-76e2c7bb0d4bc98e61d7e516c76bc3113f6ce9d7.jpg" 
+              className="w-full h-[500px] object-cover lg:rounded-tl-xl lg:rounded-bl-xl"
+            />
+            <h1 className="absolute top-4 left-4 text-3xl text-white font-bold">
+              Fornite
+            </h1>
+            <div className="absolute left-0 bottom-0 p-8 max-w-xl">
+              <p className="text-gray-200">Update</p>
+              <h2 className="text-4xl text-white font-extrabold mb-4">
+                The hunt is on Fornite Chapter 2 - Season 5: Zero point
+              </h2>
+              <button className="bg-blue-600 text-white py-4 px-6 rounded-xl flex items-center gap-2 text-lg group">
+                Play for free {" "}
+                <RiArrowRightSLine className="group-hover:translate-x-2" />
+              </button>
+            </div>
+          </div>
+          {/* Games */}
+          <div className="col-span-1 w-full h-[500px] bg-[#232323] lg:rounded-tr-xl lg:rounded-br-xl">
+            <ul className="p-4 flex flex-col gap-6 h-full overflow-y-scroll">
+              <li>
+                <a href="#" className="flex items-center gap-4 text-white text-lg">
+                  <img 
+                    src="https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg" 
+                    className="w-20 h-full object-cover"
+                    />{" "}
+                    <span className="md:hidden lg:block">Fornite</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center gap-4 text-white text-lg">
+                  <img 
+                    src="https://images.start.gg/images/videogame/14/image-c9083bae5a162d24291bd91215acf83c.jpg?ehk=XDMLbdLsmqhCbIGWQFxmW%2B%2FBVmQwR6Yipp%2F3rew5G18%3D&ehkOptimized=hgpC4xrT9w3z9%2FMUNA5wU9DFtp6y0K58JoULvRWW0%2FI%3D" 
+                    className="w-20 h-full object-cover"
+                    />{" "}
+                    <span className="md:hidden lg:block">Rocket League</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center gap-4 text-white text-lg">
+                  <img 
+                    src="https://cdn.cdkeys.com/media/catalog/product/t/h/the_escapists_2_pc_cover.jpg" 
+                    className="w-20 h-full object-cover"
+                    />{" "}
+                    <span className="md:hidden lg:block">The Escapist 2</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex items-center gap-4 text-white text-lg">
+                  <img 
+                    src="https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg" 
+                    className="w-20 h-full object-cover"
+                    />{" "}
+                    <span className="md:hidden lg:block">Fornite</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+
+
+        </div>
+        {/* Slide VideoGames */}
+        <div>
+          <h1>Games on sale</h1>
+          <div>
+            <RiArrowLeftCircleLine />
+            <RiArrowRightCircleLine />
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
